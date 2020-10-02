181c6040be1feb85a8cc8739ad2b08c199b68e59
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-02 14:02:21 +0000

    Finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Prompts user to enter positive numer, negative  * number quits
+ * @author Jaden Rancharan
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //declares user input
+    int num;
+
+    //starts loop
+    do{
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+      num = input.nextInt();
+    }while(num >= 0);
+    System.out.println("All done!");
     
   }
 }
